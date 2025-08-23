156df208f5c9d2972038e340f4550ea309e8ec3c
hieki-chan <hieuca205@gmail.com>
2025-08-23 13:10:56 +0700

    delegate-based to IMessageHandler<T> callbacks

---
+++ Assets/SimplestPubSubEver/Runtime/Internal/IMessageHandler.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Stupeak.SimplestPubSubEver
+{
+    public interface IMessageHandler
+    {
+        
+    }
+
+    public interface IMessageHandler<T> : IMessageHandler
+        where T : IMessage
+    {
+        void Invoke(T message);
+    }
+}
--- Assets/SimplestPubSubEver/Runtime/Subscribers/Subscriber.cs
+++ Assets/SimplestPubSubEver/Runtime/Subscribers/Subscriber.cs
@@ -1,11 +1,35 @@
+
+using System;
+
 namespace Stupeak.SimplestPubSubEver
 {
     public readonly struct Subscriber : ISubscriber
     {
+        public ISubscription Subscribe<T>(Action callback, Channel channel = default)
+            where T : IMessage
+        {
+            return Subscribe<T>(new CallbackMessage(callback), channel);
+        }
+
+        public ISubscription Subscribe<T>(CallbackMessage callback, Channel channel = default)
+            where T : IMessage
+        {
+            CallbackMessageHandler<T> messageHandler = new(callback);
+
+            return Subscribe(messageHandler, channel);
+        }
+
         public ISubscription Subscribe<T>(CallbackMessage<T> callbackMessage, Channel channel = default)
             where T : IMessage
         {
-            var subscription = MessageBroker.Subscribe(callbackMessage, channel);
+            CallbackMessageHandler<T> messageHandler = new(callbackMessage);
+
+            return Subscribe(messageHandler, channel);
+        }
+
+        public ISubscription Subscribe<T>(IMessageHandler<T> messageHandler, Channel channel = default) where T : IMessage
+        {
+            var subscription = MessageBroker.Subscribe(messageHandler, channel);
 
             return subscription;
         }
